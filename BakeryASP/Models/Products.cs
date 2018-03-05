b1918f4221e4fdaac97f786213eb9e819527dff8
Dana <lockwood.da88@gmail.com>
2018-03-05 12:04:49 -0800

    Products model(s)

---
+++ BakeryASP/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BakeryASP.Models;
+
+namespace BakeryASP.Models
+{
+    public class Products
+    {
+        public int ProductKey { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
