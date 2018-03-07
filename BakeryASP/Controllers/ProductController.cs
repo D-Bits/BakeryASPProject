6ec30dd2e8e3a08ebb22120f6f0e22541bb99f7d
Dana <lockwood.da88@gmail.com>
2018-03-07 11:46:12 -0800

    Product controller

---
+++ BakeryASP/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryASP.Models;
+using BakeryASP.Controllers;
+
+namespace BakeryASP.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        BakeryEntities db = new BakeryEntities();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
+ 
