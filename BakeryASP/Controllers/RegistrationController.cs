0b799aa4b5563e248a717256f5a8a1db20a438c4
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2018-03-11 19:23:56 -0700

    Registration Controller

---
+++ BakeryASP/Controllers/RegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryASP.Models;
+using BakeryASP.Controllers;
+
+/// Controller for registering new customers
+namespace BakeryASP.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        BakeryEntities db = new BakeryEntities();
+
+        public ActionResult Index()
+        {
+           
+        }
+
+    }
+}
