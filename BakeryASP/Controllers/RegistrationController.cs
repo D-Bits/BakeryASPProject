992071290beda2a042a555753a67c687c5850916
Dana <lockwood.da88@gmail.com>
2018-03-19 12:48:23 -0700

    Reg controller

--- BakeryASP/Controllers/RegistrationController.cs
+++ BakeryASP/Controllers/RegistrationController.cs
@@ -6,35 +6,47 @@ using System.Web.Mvc;
 using BakeryASP.Models;
 using BakeryASP.Controllers;
 
+
+
 /// Controller for registering new customers
+
 namespace BakeryASP.Controllers
 {
+
     public class RegistrationController : Controller
+
     {
 
         BakeryEntities db = new BakeryEntities();
 
         public ActionResult Index()
+
         {
             return View();
         }
 
         [HttpPost]
+
         [ValidateAntiForgeryToken]
-        public ActionResult(
+
+        public string ActionResult Index(
+
             [Bind(Include =
             "LastName, " +
             "FirstName, " +
             "Email, " +
             "Phone, ")
             ]Person p)
+
         {
 
             db.People.Add(p);
             db.SaveChanges();
 
             return View();
+
         }
 
     }
+
 }
