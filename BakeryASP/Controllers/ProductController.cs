76f43b6bd32e05202f64c290055f35e83624881e
Morgan <drawinggravity@gmail.com>
2018-03-19 13:30:28 -0700

    Add project files.

---
+++ BakeryASP/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryASP.Models;
+using BakeryASP.Controllers;
+
+namespace BakeryASP.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        BakeryEntities1 db = new BakeryEntities1();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
+ 
