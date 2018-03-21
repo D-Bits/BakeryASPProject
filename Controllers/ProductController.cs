0507e5607b7f48afcba1deaf3002a880cf4b945f
James Shively III <james.shively-iii@seattlecentral.edu>
2018-03-21 10:06:06 -0700

    Upload 1 of 2 for BakeryASP we did
    GitHub desktop app failed to sync my changes, so I have to do a two part update on here.  This is our protobakery application for ITC172 - ASP.NET.  M.Goldin, D.Lockwood, M.Richmond, J.Shively (Group 1).

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryASP.Models;
+
+namespace BakeryASP.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        BakeryEntities2 db = new BakeryEntities2();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
+ 
